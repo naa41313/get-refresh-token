cc3cb58a7219a1db62d6e5f0dd1d1be27fcfcf2b
e1ton1au <26325741+e1ton1au@users.noreply.github.com>
2019-12-19 15:07:32 +0800

    chore: remove unused code

--- oauth2.js
+++ oauth2.js
@@ -21,7 +21,6 @@ const opn = require('open');
 const destroyer = require('server-destroy');
 
 const {google} = require('googleapis');
-const plus = google.plus('v1');
 
 /**
  * To use OAuth2 authentication, we need access to a a CLIENT_ID, CLIENT_SECRET, AND REDIRECT_URI.  To get these credentials for your application, visit https://console.cloud.google.com/apis/credentials.
